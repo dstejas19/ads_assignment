0fa3da2319c966e75911baf42a878e442dc7386c
Tejas Dharmastalam Sridhar <tejas@live.in>
2022-11-12 02:44:02 -0800

    testing delete

--- Assignment_1/main.cc
+++ Assignment_1/main.cc
@@ -27,63 +27,67 @@ int main()
 
     std::cout<<"New Tree Initialized"<<std::endl;
 
-    // avlTree->insert(1);
-    // avlTree->insert(3);
-    // avlTree->insert(7);
-    // avlTree->insert(6);
-    // avlTree->insert(4);
-    // avlTree->insert(5);
-    // avlTree->insert(2);
-
-    avlTree->insert(10);
+    avlTree->insert(1);
+    avlTree->insert(3);
     avlTree->insert(7);
-    avlTree->insert(3);
-    avlTree->insert(3);
+    avlTree->insert(6);
+    avlTree->insert(4);
     avlTree->insert(5);
-    avlTree->insert(4);
-    avlTree->insert(4);
     avlTree->insert(2);
-    avlTree->insert(12);
-    avlTree->insert(16);
-    avlTree->insert(13);
-    avlTree->insert(19);
-    avlTree->insert(22);
-    avlTree->insert(25);
-    avlTree->insert(17);
 
-    // avlTree->print();
+    avlTree->remove(6);
+    avlTree->remove(7);
+    avlTree->remove(5);
 
-    std::cout<<"Search starts"<<std::endl;
+    // avlTree->insert(10);
+    // avlTree->insert(7);
+    // avlTree->insert(3);
+    // avlTree->insert(3);
+    // avlTree->insert(5);
+    // avlTree->insert(4);
+    // avlTree->insert(4);
+    // avlTree->insert(2);
+    // avlTree->insert(12);
+    // avlTree->insert(16);
+    // avlTree->insert(13);
+    // avlTree->insert(19);
+    // avlTree->insert(22);
+    // avlTree->insert(25);
+    // avlTree->insert(17);
 
-    print(avlTree->search(5));
-    print(avlTree->search(-3));
-    print(avlTree->search(100));
-    print(avlTree->search(8));
-    print(avlTree->search(10));
-    print(avlTree->search(7));
-    print(avlTree->search(3));
-    print(avlTree->search(5));
-    print(avlTree->search(4));
-    print(avlTree->search(2));
-    print(avlTree->search(12));
-    print(avlTree->search(16));
-    print(avlTree->search(13));
-    print(avlTree->search(19));
-    print(avlTree->search(22));
-    print(avlTree->search(25));
-    print(avlTree->search(17));
+    avlTree->print();
 
-    std::cout<<"Search with bounds starts"<<std::endl;
+    // std::cout<<"Search starts"<<std::endl;
 
-    print(avlTree->search(10, 23));
+    // print(avlTree->search(5));
+    // print(avlTree->search(-3));
+    // print(avlTree->search(100));
+    // print(avlTree->search(8));
+    // print(avlTree->search(10));
+    // print(avlTree->search(7));
+    // print(avlTree->search(3));
+    // print(avlTree->search(5));
+    // print(avlTree->search(4));
+    // print(avlTree->search(2));
+    // print(avlTree->search(12));
+    // print(avlTree->search(16));
+    // print(avlTree->search(13));
+    // print(avlTree->search(19));
+    // print(avlTree->search(22));
+    // print(avlTree->search(25));
+    // print(avlTree->search(17));
 
-    print(avlTree->search(2, 25));
+    // std::cout<<"Search with bounds starts"<<std::endl;
 
-    print(avlTree->search(5, 17));
+    // print(avlTree->search(10, 23));
 
-    print(avlTree->search(-5, 1));
+    // print(avlTree->search(2, 25));
 
-    print(avlTree->search(100, 1011));
+    // print(avlTree->search(5, 17));
+
+    // print(avlTree->search(-5, 1));
+
+    // print(avlTree->search(100, 1011));
 
     return 0;
 }
